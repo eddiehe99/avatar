8832d34bb50925a70025964b02030e548991218c
Eddie-He-090 <40681449+Eddie-He-090@users.noreply.github.com>
2025-01-22 10:07:32 +0800

    Merge remote-tracking branch 'upstream/main'

--- app/page.tsx
+++ app/page.tsx
@@ -1,63 +1,61 @@
 "use client"
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useLayoutEffect } from 'react';
 
 import { AvatarCollection } from "@/components/avatar-collection"
 import { Sidebar } from "@/components/sidebar"
 
 import { collections } from "@/data/collections"
 
+import dynamic from 'next/dynamic';
+
+// 动态加载 MyComponent，并禁用 SSR
+const AutoWidthComponent = dynamic(() => import('@/components/auto-width-component'), {
+  ssr: false,
+});
+
 
 export default function Page() {
 
-  const [gridWidth, setGridWidth] = useState<number>(1024);
+  // const [gridWidth, setGridWidth] = useState<number>(1024);
 
-  useEffect(() => {
-    const updateGridWidth = () => {
-      const gridContainer = document.getElementById('gridContainer');
-      if (gridContainer) {
-        setGridWidth(gridContainer.getBoundingClientRect().width);
-        console.log(`gridWidth: ${gridWidth}`);
-      }
-    };
+  // useLayoutEffect(() => {
+  //   // 确保代码只在客户端运行
+  //   if (typeof window !== 'undefined') {
+  //     const updateGridWidth = () => {
+  //       const gridContainer = document.getElementById('gridContainer');
+  //       if (gridContainer) {
+  //         const newWidth = gridContainer.getBoundingClientRect().width;
+  //         setGridWidth(newWidth);
+  //         console.log(`gridWidth: ${newWidth}`);
+  //       }
+  //     };
 
-    // 添加事件监听器
-    window.addEventListener('resize', updateGridWidth);
+  //     // 添加事件监听器
+  //     window.addEventListener('resize', updateGridWidth);
 
-    // 初始化检查
-    updateGridWidth();
+  //     // 初始化检查
+  //     updateGridWidth();
 
-    // 组件卸载时移除事件监听器
-    return () => window.removeEventListener('resize', updateGridWidth);
-  });
+  //     // 组件卸载时移除事件监听器
+  //     return () => window.removeEventListener('resize', updateGridWidth);
+  //   }
+  // });
 
-  const widthStyle: React.CSSProperties = {
-    // 'lg' minimum width: 1024px
-    width: gridWidth < 1024 ? `${gridWidth}px` : 'auto',
-  };
+  // const widthStyle: React.CSSProperties = {
+  //   // 'lg' minimum width: 1024px
+  //   width: gridWidth < 1024 ? `${gridWidth}px` : 'auto',
+  // };
 
   return (
     <>
       <div className="border-t">
-        <div className="bg-background">
-          <div className="grid lg:grid-cols-5" id="gridContainer">
-            <Sidebar className="hidden lg:block" />
-            <div className="col-span-3 lg:col-span-4 lg:border-l" style={widthStyle}>
-              <div className="h-full px-4 py-6 lg:px-8 sm:px-0 md:px-2">
-                {collections.map((collection) => (
-                  <AvatarCollection
-                    key={collection.name}
-                    collection={collection}
-                    aspectRatio="square"
-                    width={150}
-                    height={150}
-                    className="w-[150px]"
-                  />
-                ))}
-              </div>
-            </div>
-          </div>
+        {/* <div className="bg-background"> */}
+        <div className="grid lg:grid-cols-5" id="gridContainer">
+          <Sidebar className="hidden lg:block" />
+          <AutoWidthComponent />
         </div>
+        {/* </div> */}
       </div>
     </>
   )
---
+++ components/auto-width-component.tsx
@@ -0,0 +1,53 @@
+import React, { useState, useLayoutEffect } from 'react';
+import { AvatarCollection } from './avatar-collection';
+import { Collection, collections } from '@/data/collections';
+
+export default function autoWidthComponent() {
+
+    const [gridWidth, setGridWidth] = useState<number>(1024);
+
+    useLayoutEffect(() => {
+        // 确保代码只在客户端运行
+        if (typeof window !== 'undefined') {
+            const updateGridWidth = () => {
+                const gridContainer = document.getElementById('gridContainer');
+                if (gridContainer) {
+                    const newWidth = gridContainer.getBoundingClientRect().width;
+                    setGridWidth(newWidth);
+                    console.log(`gridWidth: ${newWidth}`);
+                }
+            };
+
+            // 添加事件监听器
+            window.addEventListener('resize', updateGridWidth);
+
+            // 初始化检查
+            updateGridWidth();
+
+            // 组件卸载时移除事件监听器
+            return () => window.removeEventListener('resize', updateGridWidth);
+        }
+    });
+
+    const widthStyle: React.CSSProperties = {
+        // 'lg' minimum width: 1024px
+        width: gridWidth < 1024 ? `${gridWidth}px` : 'auto',
+    };
+
+    return (
+        <div className="col-span-3 lg:col-span-4 lg:border-l" style={widthStyle}>
+            <div className="h-full px-4 py-6 lg:px-8 sm:px-0 md:px-2">
+                {collections.map((collection: Collection) => (
+                    <AvatarCollection
+                        key={collection.name}
+                        collection={collection}
+                        aspectRatio="square"
+                        width={150}
+                        height={150}
+                        className="w-[150px]"
+                    />
+                ))}
+            </div>
+        </div>
+    );
+}
